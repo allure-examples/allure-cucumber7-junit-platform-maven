f0ba5f5da1f93f9e4389b635fe9ad1b567c20fbf
Yuri <yur_yurch@mail.ru>
2023-10-27 07:17:04 +0300

    mvnw added, workflow uses mvnw, pom upd

--- src/test/java/io/qameta/allure/examples/cucumber7/CucumberTest.java
+++ src/test/java/io/qameta/allure/examples/cucumber7/CucumberTest.java
@@ -1,6 +1,9 @@
 package io.qameta.allure.examples.cucumber7;
 
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 
